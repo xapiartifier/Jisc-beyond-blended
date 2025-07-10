c40705e4ff8c93f8c80caddd8082c7da8125f4af
Jim Harris <xapiartificer@gmail.com>
2025-07-10 17:51:33 +0100

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -1,9 +1,9 @@
 from langchain.document_loaders import TextLoader
 from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 
 loader = TextLoader("docs/sample_beyond_blended.txt")
 docs = loader.load()
 
-db = FAISS.from_documents(docs, OpenAIEmbeddings())
+db = FAISS.from_documents(docs, HuggingFaceEmbeddings())
 db.save_local("vectorstore")
